851d2ac9857c64b1ef532b4a08dff50f5e527cb3
aa <aa@163.com>
2016-06-20 20:42:46 +0800

    8

---
+++ Foundation-(NSNumber)/Foundation-(NSNumber)/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  Foundation-(NSNumber)
+//
+//  Created by qingyun on 16/4/5.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // NSNumber 封装基本数据类型 是NSValue的子类
+        int a1=11;
+        float a2=21.2;
+        double a3=21.2121;
+        char a4='d';
+        BOOL a5=YES;
+
+        NSNumber *iNum=[NSNumber numberWithInt:a1];
+        NSNumber *fNum=[NSNumber numberWithFloat:a2];
+        NSNumber *dNum=[NSNumber numberWithDouble:a3];
+        NSNumber *cNum=[NSNumber numberWithChar:a4];
+        NSNumber *bNum=[NSNumber numberWithBool:a5];
+        NSLog(@"%@,%@,%@,%@,%@",iNum,fNum,dNum,cNum,bNum);
+        //拆箱
+        NSLog(@"%d,%g,%g,%c,%d",[iNum intValue],[fNum floatValue],[dNum doubleValue],[cNum charValue],[bNum boolValue]);
+        long long l1=2123123123212;
+        // 装箱
+        NSNumber *llNum=[NSNumber numberWithLongLong:l1];
+        NSLog(@"llNum=%@,%lld",llNum,[llNum longLongValue]);
+        //总结：
+        //基本数据类型的封装交给NSNumber,其他数据类型交给NSValue
+    }
+    return 0;
+}
+
